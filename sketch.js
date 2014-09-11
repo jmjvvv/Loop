5a065104133646a8e8750af1575df7d56a00f91c
jmjvvv <jmjvvv@gmail.com>
2014-09-11 16:03:27 +0900

    d

--- sketch.js
+++ sketch.js
@@ -1,30 +1,23 @@
 function setup() {
   createCanvas(640, 480);
-  background(128, 0, 64);
+  
   noFill();
 
   stroke(255, 0, 0);
 
-/*
-  var x;
-
-  x = 1;
-  ellipse(x * 100, 100, 50, 50);
-
-  x = 2;
-  ellipse(x * 100, 100, 50, 50);
-
-  x = 3;
-  ellipse(x * 100, 100, 50, 50);
-  */
-  for (var x = 1; x <= 8; x++)
-  {
-    ellipse(x * 20, x * 70 + 100, 20, 30);
-  }
 }
-function draw() {
-  background(128, 0, 64);
 
-  ellipse(320, 240, mouseX, mouseY);
+function draw () {
+
+   background(128, 0, 64);
+
+for (var x = 1; x <= 8; x++)
+{
+  ellipse(x * 70, x * 20 + 100, x+50, x*10);
 }
+
+var size =    Math.abs(mouseX - 320);
+
+ellipse(320, 240, mouseX, mouseY);
+
 }
