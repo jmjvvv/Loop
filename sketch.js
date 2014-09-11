70bd64d27f23f47140e180f1233a19053864c243
jmjvvv <jmjvvv@gmail.com>
2014-09-11 15:19:14 +0900

    D

--- sketch.js
+++ sketch.js
@@ -7,12 +7,12 @@ function setup() {
 
   var x;
 
-  x = 100;
-  ellipse(x, 100, 50, 50);
+  x = 1;
+  ellipse(x * 100, 100, 50, 50);
 
-  x = 250;
-  ellipse(x, 100, 50, 50);
+  x = 2;
+  ellipse(x * 100, 100, 50, 50);
 
-  x = 300;
-  ellipse(x, 100, 50, 50);
+  x = 3;
+  ellipse(x * 100, 100, 50, 50);
 }
